3c99643d22147e6fdfdb983e5d6a5e071cbb7103
diwashkafle <diwashhkafle66@gmail.com>
2021-10-21 14:41:47 +0545

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let home = document.getElementById("home");
 let about = document.getElementById("about")
+let homePage = document.querySelector(".inside-box")
+let aboutPage = document.querySelector(".inside-box2")
 
 window.addEventListener("load",function(){
+
+
 home.addEventListener("mouseover",function(){
     home.src="./images/home2.svg"
     
@@ -21,5 +25,23 @@ about.addEventListener("mouseout",function(){
    
 
 })
+home.addEventListener("click",()=>{
+    console.log("home")
+    if(homePage.style.display==="none"){
+        homePage.style.animationName="forHomePage";
+    
+    }
+    homePage.style.display="flex";
+    aboutPage.style.display="none";
+   
+})
+about.addEventListener("click",()=>{
+    console.log("about")
+    aboutPage.style.display="flex";
+    homePage.style.display="none";
+    aboutPage.style.animationName="forAboutPage";
+    
+})
+
 
 })
